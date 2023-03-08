f46d2b75f49640010254d7ed282739af0dbed451
Pero Schwitalla <g00422221@atu.ie>
2023-03-08 18:31:36 +0000

    added basic npc and changed grid from numbers to x and os

--- TicTacToe/Source.c
+++ TicTacToe/Source.c
@@ -9,27 +9,62 @@
 #include<string.h>
 
 
-
-void RefreshGrid(int* gridPointer);
+void PlayerTurn(char * gridPointer);
+void RefreshGrid(char* gridPointer);
+void NPCTurn(char* gridPointer);
 
 int main() {
 	//declaring variables
-	int gridArray[3][3] = { 0 }, i, j,k=1, gridPosition;
-	int* gridPointer = &gridArray[0];
-
+	char gridArray[3][3] = {'#','#','#' ,'#' ,'#' ,'#' ,'#' ,'#' ,'#' }, i, j, k=1, n = 0;
+	char* gridPointer = &gridArray[0];
+	int gridPosition;
+	for (i = 0; i < 5; i++)
+	{
+		RefreshGrid(gridPointer);
+		PlayerTurn(gridPointer);
+		RefreshGrid(gridPointer);
+		NPCTurn(gridPointer);
 
 	
-	while (k==1)
+	}
+
+	return 0;
+
+}
+
+
+//clear console and prints out the grid
+void RefreshGrid(char* gridPointer)
+{
+	int j, k, n=0;
+	system("cls");
+	for (j = 0; j < 3; j++)
 	{
-		printf("enter a position on the grid (0-9)");
+		for (k = 0; k < 3; k++)
+		{
+			printf("%c ", *(gridPointer + n));
+			n++;
+		}
+		printf("\n");
+	}
+}
+
+//promts player for a position and checks if its valid
+void PlayerTurn(char* gridPointer)
+{
+	
+	int k = 1, gridPosition;
+	while (k == 1)
+	{
+		printf("enter a position on the grid (0-8)");
 		scanf("%i", &gridPosition);
 		printf("\n");
-		
-		if (*(gridPointer+gridPosition) == 0)
+
+		if (*(gridPointer + gridPosition) == 35)
 		{
-			*(gridPointer + gridPosition) = 1;
+			*(gridPointer + gridPosition) = 88;
 			RefreshGrid(gridPointer);
-			k = 1;
+			k = 0;
 
 		}
 		else
@@ -38,25 +73,25 @@ int main() {
 			printf("\n");
 		}
 	}
-		
-	return 0;
-
 }
 
-
-//clear console and prints out the grid
-void RefreshGrid(int* gridPointer)
+//npc opponent. generates randomly pick a grid location for now
+void NPCTurn(char* gridPointer)
 {
-	int j, k, n=0;
-	system("cls");
-	for (j = 0; j < 3; j++)
+	
+	int k = 1, gridPosition;
+
+	while (k == 1)
 	{
-		for (k = 0; k < 3; k++)
+		srand(time(NULL));
+		gridPosition = rand() % 9;
+		printf("%i \n", gridPosition);
+
+		if (*(gridPointer + gridPosition) == 35)
 		{
-			printf("%i ", *(gridPointer + n));
-			n++;
+			*(gridPointer + gridPosition) = 79;
+			RefreshGrid(gridPointer);
+			k = 0;
 		}
-		printf("\n");
 	}
 }
-
